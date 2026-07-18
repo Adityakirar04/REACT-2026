107c00e8d5ca0dd16da8c66d9f1a40d214f96e9d
Adityakirar04 <mr.kirar62@gmail.com>
2026-07-18 22:56:20 +0530

    ok

--- 09usestate/src/App.jsx
+++ 09usestate/src/App.jsx
@@ -1,27 +1,60 @@
- import React, { useState } from 'react'
+ import { useState } from "react";
+import "./App.css";
 
 const App = () => {
-
-  const [num, setNum] = useState(0)
+  const [num, setNum] = useState(0);
 
   function increaseNum() {
-    setNum(num+1)
+    setNum(num + 1);
   }
+
   function decreaseNum() {
-    setNum(num-1)
+    setNum(num - 1);
   }
+
   function jump5Num() {
-    setNum(num+5)
+    setNum(num + 5);
+  }
+
+  function resetNum() {
+    setNum(0);
   }
 
   return (
-    <div>
-      <h1>{num}</h1>
-      <button onClick={increaseNum}>increase</button>
-      <button onClick={decreaseNum}>decrease</button>
-      <button onClick={jump5Num}>Increas by 5</button>
-    </div>
-  )
-}
+    <div className="container">
 
-export default App
+      <div className="card">
+
+        <h1>Counter App</h1>
+
+        <div className="counter">
+          {num}
+        </div>
+
+        <div className="buttons">
+
+          <button className="green" onClick={increaseNum}>
+            +1
+          </button>
+
+          <button className="red" onClick={decreaseNum}>
+            -1
+          </button>
+
+          <button className="blue" onClick={jump5Num}>
+            +5
+          </button>
+
+          <button className="gray" onClick={resetNum}>
+            Reset
+          </button>
+
+        </div>
+
+      </div>
+
+    </div>
+  );
+};
+
+export default App;
